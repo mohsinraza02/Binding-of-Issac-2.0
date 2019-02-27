3fa7db4b28a7fa34fd6a2a51bf53cf899cd8bb79
hafsazia <47464997+hafsazia@users.noreply.github.com>
2019-02-26 19:02:51 -0700

    Update Item.java

--- Item.java
+++ Item.java
@@ -6,10 +6,16 @@ public class Item {
 	private int type; // location in player array
 	private double time; // time taken from total map time
 	
-	/* Javadoc in process
-	*
-	
-	*/
+	/**
+        * This constructor takes the 4 parameter values and sets the first parameter
+        * to the instance variable name, the second parameter to the instance
+        * variable value,the third parameter to the instance variable type and the fourth parameter
+	* to the instance variable time.
+        * @param a this parameter is set to the instance variable name
+        * @param b this parameter is set to the instance variable value
+	* @param c this parameter is set to the instance variable type
+	* @param d this parameter is set to the instance variable time
+        */
 	public Item(String a, int b, int c, double d) {
 		this.name = a;
 		this.value = b;
@@ -17,21 +23,36 @@ public class Item {
 		this.time = d;
 	}
 	
-	/* Javadoc in process
-	* 
-	*
-	*/
-	// new get methods for each of the variables
-	
+
+	/**
+        * The method getName returns the instance variable name of type String from the class
+        * Item.
+        * @return instance variable name
+        */
 	public Item getName(){
 		return this.name;
 	}
+	/**
+        * The method getValue returns the instance variable value of type int from the class
+        * Item.
+        * @return instance variable value
+        */
 	public Item getValue(){
 		return this.value;
 	}
+	/**
+        * The method getType returns the instance variable type of type int from the class
+        * Item.
+        * @return instance variable type
+        */
 	public Item getType(){
 		return this.type;
 	}
+	/**
+        * The method getTime returns the instance variable time of type double from the class
+        * Item.
+        * @return instance variable time
+        */
 	public Item getTime(){
 		return this.time;
 	}
