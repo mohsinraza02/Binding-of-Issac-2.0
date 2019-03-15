59f0df7b355eb3f98bcf46a30011ad51f57391e3
friedchickean <47467633+friedchickean@users.noreply.github.com>
2019-03-14 20:52:22 -0600

    Delete Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Test {
-	
-	public static void main(String[] args) {
-		ArrayList<Item>[] room = new ArrayList[5];
-		room[0] = new ArrayList<Item>(3);
-	}
-}
