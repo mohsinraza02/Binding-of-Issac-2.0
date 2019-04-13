023a7a5db38acdb4b9a4e867fa3866b55ff2b0a0
hafsazia <47464997+hafsazia@users.noreply.github.com>
2019-04-12 20:03:06 -0600

    Update HUDobjects.java

--- codes/HUDobjects.java
+++ codes/HUDobjects.java
@@ -1,7 +1,19 @@
 package codes;
 
 public class HUDobjects extends Entities {
-
+/**
+         * This constructor takes 5 parameter values.
+	 * 
+	 * @param x 
+	 * 
+	 * @param y 
+	 *
+	 * @param  w
+	 *
+	 * @param h 
+	 *
+	 * @param image 
+         */
 	public HUDobjects(int x, int y, int w, int h, String image) {
 		super(x, y, w, h, "HUD", image);
 		// TODO Auto-generated constructor stub
